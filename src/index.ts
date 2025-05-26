a65fb17582886ab0e9f40413e8f54bf3228fe152
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-05-27 00:16:16 +0530

    all code in index.js beacuse other class were not giving types in js

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,62 @@
 import {NextFunction, Request, Response} from "express";
-import TokenClass from "./utils/jwt";
 import "dotenv/config";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+interface IVerified{
+    verified:boolean,
+    data:{
+        userId:string|number;
+        data?:object;
+        iat: number;
+        exp: number;
+    }
+}
 /**
  * Class to handle user authentication using JWT and password hashing
  * @class UserAuth
  * @extends TokenClass
  */
-class UserAuth extends TokenClass{
+class UserAuth{
+    private authSecret:string
     constructor(){
-        super(process.env.AUTH_SECRET!);
+        this.authSecret=process.env.AUTH_SECRET!
+    }
+    /**
+     * Creates a JWT token with the provided data and expiration time
+     * @param {{userId:string|number,data:object}} data - Data to be included in the token
+     * @param {number} time - Expiration time in minutes
+     * @returns {Promise<String>} - Returns a promise that resolves to the created token
+     */
+    public async createToken(data:{userId:string|number,data:object},time:number):Promise<string>{
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const token=await jwt.sign(data,this.authSecret,{expiresIn:`${time}m`}); //time in minutes
+                resolve(token);
+            }catch(err:any){
+                reject(err);
+            }
+        })
+    }
+    /**
+     * Verifies the provided JWT token
+     * @param {string} token - The JWT token to verify
+     * @returns {Promise<IVerified>} - Returns a promise that resolves to an object containing verification status and data
+     */
+    public async verifyToken(token:string):Promise<IVerified>{
+        return new Promise(async(resolve,reject)=>{
+            try {
+                const isVerified=await jwt.verify(token,this.authSecret);
+                if(!isVerified) reject(new Error("Token expires or invalid"));
+                const data=JSON.parse(JSON.stringify(isVerified));
+                resolve({
+                    verified:true,
+                    data
+                });
+            } catch (err:any) {
+                reject(err);
+            }
+        })
     }
     /**
      * Middleware to authenticate user requests using JWT token
@@ -30,7 +76,7 @@ class UserAuth extends TokenClass{
                 })
                 return;
             }
-            const tokenM=new TokenClass(process.env.AUTH_SECRET!);
+            const tokenM=new UserAuth();
             const data=await tokenM.verifyToken(authToken);         
             req.userId=data.data.userId;
             next();
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import TokenClass from "../dist/utils/jwt";
+interface IVerified {
+    verified: boolean;
+    data: {
+        userId: string | number;
+        data?: object;
+        iat: number;
+        exp: number;
+    };
+}
 declare class UserAuth extends TokenClass {
     constructor();
+    createToken(data: {userId:string|number,data:object}, time: number): Promise<string>;
+    verifyToken(token: string): Promise<IVerified>;
     auth(req: Request, res: Response, next: NextFunction): Promise<void>;
     hashPassword(password: string): Promise<string>;
     comparePassword(password: string, hashedPassword: string): Promise<boolean>;
