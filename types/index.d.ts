96a769254a4c0aacad6bcb1dfd9a00a98727aa6c
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-26 11:54:01 +0530

    updated methods

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,5 +3,7 @@ import TokenClass from "../dist/utils/jwt";
 declare class UserAuth extends TokenClass {
     constructor();
     auth(req: Request, res: Response, next: NextFunction): Promise<void>;
+    hashPassword(password: string): Promise<string>;
+    comparePassword(password: string, hashedPassword: string): Promise<boolean>;
 }
 export default UserAuth;
